5246ffbfae7de8976eb423af7c9627e2c7672511
Quentin Boyer <qbsecond@gmail.com>
2019-11-29 12:42:51 +0100

    It is quite good

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rustyline::{error::ReadlineError, Editor};
+use std::collections::HashMap;
 #[macro_use]
 extern crate lalrpop_util;
 
@@ -67,7 +68,9 @@ fn not(expr: LogicalExpr) -> LogicalExpr {
             variable,
             expr: Box::new(not(*expr)),
         },
-        p => LogicalExpr::Not(Box::new(p)),
+        LogicalExpr::Predicate { name, args } => {
+            LogicalExpr::Not(Box::new(LogicalExpr::Predicate { name, args }))
+        }
     }
 }
 
@@ -99,21 +102,34 @@ fn desugar(expr: LogicalExpr) -> LogicalExpr {
                 right: left,
             })),
         },
-        LogicalExpr::Bin { op, left, right } => LogicalExpr::Bin {
-            op,
-            left: Box::new(desugar(*left)),
-            right: Box::new(desugar(*right)),
+        e => recursive_apply(e, desugar),
+    }
+}
+
+fn sieve_not_down(expr: LogicalExpr) -> LogicalExpr {
+    match expr {
+        LogicalExpr::Not(e) => not(*e),
+        e => recursive_apply(e, sieve_not_down),
+    }
+}
+
+fn recursive_apply(expr: LogicalExpr, func: impl Fn(LogicalExpr) -> LogicalExpr) -> LogicalExpr {
+    match expr {
+        LogicalExpr::Not(e) => LogicalExpr::Not(Box::new(func(*e))),
+        LogicalExpr::Predicate { name, args } => LogicalExpr::Predicate { name, args },
+        LogicalExpr::Exists { variable, expr } => LogicalExpr::Exists {
+            variable,
+            expr: Box::new(func(*expr)),
         },
         LogicalExpr::ForAll { variable, expr } => LogicalExpr::ForAll {
             variable,
-            expr: Box::new(desugar(*expr)),
+            expr: Box::new(func(*expr)),
         },
-        LogicalExpr::Exists { variable, expr } => LogicalExpr::Exists {
-            variable,
-            expr: Box::new(desugar(*expr)),
+        LogicalExpr::Bin { op, left, right } => LogicalExpr::Bin {
+            op,
+            left: Box::new(func(*left)),
+            right: Box::new(func(*right)),
         },
-        LogicalExpr::Not(e) => LogicalExpr::Not(Box::new(desugar(*e))),
-        LogicalExpr::Predicate { name, args } => LogicalExpr::Predicate { name, args },
     }
 }
 
@@ -178,20 +194,76 @@ impl std::fmt::Display for LogicalExpr {
 
 lalrpop_mod!(pub syntax);
 
-macro_rules! parse_formula {
-    ($f:expr) => {
-        match $f {
-            Some(formula) => match syntax::ExprParser::new().parse(&formula) {
-                Ok(f) => f,
-                Err(e) => {
-                    eprintln!("Invalid formula: {}", e);
+pub enum Input {
+    Binding(String),
+    Formula(LogicalExpr),
+}
+
+pub enum Command {
+    Help,
+    Exit,
+    Sieve(Input),
+    Render(Input),
+    Desugar(Input),
+    Bind{name: String, command: Box<Command>},
+}
+
+impl Command {
+    fn is_bindable(&self) -> bool {
+        match self {
+            Command::Help => false,
+            Command::Exit => false,
+            Command::Bind{..} => false,
+            Command::Sieve(_) => true,
+            Command::Render(_) => true,
+            Command::Desugar(_) => true,
+        }
+
+    }
+    fn name(&self) -> &'static str{
+        match self {
+            Command::Help => "help",
+            Command::Exit => "exit",
+            Command::Bind{..} => "bind",
+            Command::Sieve(_) => "sieve",
+            Command::Render(_) => "render",
+            Command::Desugar(_) => "desugar",
+        }
+    }
+    fn print_usage() {
+        println!("Help:");
+        println!(
+            "    {} <formula>: removes all <=> and => in f",
+            Paint::new("desugar").bold()
+        );
+        println!(
+            "    {} <formula>: pretty render of the forumula",
+            Paint::new("render").bold()
+        );
+        println!(
+            "    {} <formula>: transfer all not operators to the litterals",
+            Paint::new("sieve").bold()
+        );
+        println!(
+            "    {} <name> <command>: bind the result of command to name, you can refer to this formula by name in inputs",
+            Paint::new("bind").bold()
+        );
+        println!("    {}: print this help", Paint::new("help").bold());
+        println!("    {}: exit", Paint::new("exit").bold());
+    }
+}
+
+macro_rules! resolve_input {
+    ($registry:expr, $input:expr) => {
+        match $input {
+            Input::Formula(e) => e,
+            Input::Binding(name) => match $registry.get(&name) {
+                Some(e) => e.clone(),
+                None => {
+                    eprintln!("No such binding: {}", name);
                     continue;
                 }
             },
-            None => {
-                eprintln!("No formula was provided !");
-                continue;
-            }
         }
     };
 }
@@ -201,24 +273,47 @@ fn main() {
     if rl.load_history("history.txt").is_err() {
         println!("No previous history.");
     }
+    let mut bindings: HashMap<String, LogicalExpr> = HashMap::new();
     loop {
         let readline = rl.readline(">> ");
         match readline {
-            Ok(mut line) => {
+            Ok(line) => {
                 rl.add_history_entry(line.as_str().trim());
-                let formula = line.find(" ").map(|i| line.split_off(i));
-                match line.trim() {
-                    "help" => {
-                        println!("Help:");
-                        println!("    {} f: removes all <=> and => in f", Paint::new("desugar").bold());
-                        println!("    {} f: pretty render of the forumula", Paint::new("render").bold());
-                        println!("    {}: print this help", Paint::new("help").bold());
-                        println!("    {}: exit", Paint::new("exit").bold());
+                if line.starts_with("DBG") {
+                    dbg!(&bindings);
+                    continue;
+                }
+                let command = match syntax::CommandParser::new().parse(&line) {
+                    Err(e) => {
+                        eprintln!("Invalid command: {}", e);
+                        continue;
                     }
-                    "exit" => break,
-                    "desugar" => println!("{}", desugar(parse_formula!(formula))),
-                    "render" => println!("{}", parse_formula!(formula)),
-                    s => eprintln!("Invalid command: {}", s),
+                    Ok(c) => c,
+                };
+                match command {
+                    Command::Help => Command::print_usage(),
+                    Command::Exit => break,
+                    Command::Desugar(expr) => {
+                        println!("{}", desugar(resolve_input!(&bindings, expr)))
+                    }
+                    Command::Render(expr) => println!("{}", resolve_input!(&bindings, expr)),
+                    Command::Sieve(expr) => {
+                        println!("{}", sieve_not_down(resolve_input!(&bindings, expr)))
+                    }
+                    Command::Bind{name ,command} => {
+                        if !command.is_bindable() {
+                            eprintln!("Command {} is not bindable", command.name());
+                            continue
+                        }
+                        let result = match *command {
+                            Command::Render(expr) => resolve_input!(&bindings, expr),
+                            Command::Sieve(expr) => sieve_not_down(resolve_input!(&bindings, expr)),
+                            Command::Desugar(expr) => desugar(resolve_input!(&bindings, expr)),
+                            c => unreachable!("tried binding invalid command: {}", c.name()),
+                        };
+                        println!("{}", result);
+                        bindings.insert(name, result);
+                    }
                 }
             }
             Err(ReadlineError::Interrupted) => {
